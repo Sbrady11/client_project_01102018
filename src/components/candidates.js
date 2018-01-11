047ec9679af5de160c13ae1457c78c1c04a4b86f
Simon Brady <simonbrady@mac.com>
2018-01-11 18:36:44 -0500

    Style paragraphs

--- src/components/candidates.js
+++ src/components/candidates.js
@@ -5,15 +5,16 @@ class Candidates extends React.Component {
   render() {
     return (
       <div>
-      <p>
-        CANDIDATES
-        </p>
         <p>
-Did you know that the average speed your resume is screened by the corporate recruiter is 7 seconds or less?
-
-Having been in the corporate world for multiple years, our staff consultants know how to help you market your resume to the role of your choice. Secondly, an ally for you should be someone who is transparent about your skills, knows the industry you are in or has colleagues that have personally experienced it. Chances are no one recruiter can be all you have in obtaining the role or new career. We are in the business of aligning relations
-</p>
-        </div>
+          CANDIDATES
+        </p>
+        <p className="page-text">
+          Did you know that the average speed your resume is screened by the corporate recruiter is 7 seconds or less?
+        </p>
+        <p className="page-text">
+          Having been in the corporate world for multiple years, our staff consultants know how to help you market your resume to the role of your choice. Secondly, an ally for you should be someone who is transparent about your skills, knows the industry you are in or has colleagues that have personally experienced it. Chances are no one recruiter can be all you have in obtaining the role or new career. We are in the business of aligning relations
+        </p>
+      </div>
     );
   }
 }
