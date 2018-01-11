3b4ce3c24f5e69c45f9bbcd4be4826ac451d1dd9
Simon Brady <simonbrady@mac.com>
2018-01-11 18:24:43 -0500

    Add content to components, next step to make into pages

---
+++ src/components/employers.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Employers extends React.Component {
+  render() {
+    return (
+      <div>
+      <p>
+        EMPLOYERS
+        </p>
+        <p>
+
+Today, candidates are going to be at an advantage over employers for the first time in hiring history; as recent laws have been passed by the government that affects the hiring and decision making compensation practices.
+
+This will affect compensation practices, budgets, salaries, and limit your abilities to hire and compensate fairly across your current team. It may even waste your time from an interviewing standpoint.
+
+Why??
+
+Human Resource Departments and all Staffing Agencies that are involved in representing candidates for your singular openings will not be allowed by law to ask a candidate their current compensation.
+
+ATP Options has the end to end staffing solutions /choices that will help you navigate through these challenging changes. Project Staffing will be the wave of the future just like Siri and cloud-based technology
+        </p>
+        </div>
+    );
+  }
+}
+
+export default Employers;
