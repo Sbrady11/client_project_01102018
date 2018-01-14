1f0c0180ed31e8aa9175460cd764a3169a231a7a
Simon Brady <simonbrady@mac.com>
2018-01-14 15:50:39 -0500

    Add sales page:

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import AboutUs from '../components/aboutUs.js';
 import Employers from '../components/employers.js';
 import Candidates from '../components/candidates.js';
 import Home from '../components/home.js';
+import Sales from '../components/sales.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import AnimatedRoutes from "../components/animatedRoutes";
 
@@ -15,16 +16,18 @@ class App extends Component {
         <Router>
           <div>
             <div className="TopBar">
-                <Link to="/" activeClassName="active">ATPOptions</Link>
-                <Link to="/about" activeClassName="active">About</Link>
-                <Link to="/employers" activeClassName="active">Employers</Link>
-                <Link to="/candidates" activeClassName="active">Candidates</Link>
+                <Link to="/">ATPOptions</Link>
+                <Link to="/about">About</Link>
+                <Link to="/employers">Employers</Link>
+                <Link to="/candidates">Candidates</Link>
+                <Link to="/sales">Sales</Link>
             </div>
             <AnimatedRoutes>
               <Route path="/" exact component={Home} />
               <Route path="/about" exact component={AboutUs} />
               <Route path="/employers" exact component={Employers} />
               <Route path="/candidates" exact component={Candidates} />
+              <Route path="/sales" exact component={Sales} />
             </AnimatedRoutes>
           </div>
         </Router>
