92cf1d8747bfb0b649626d713bca243618ed7859
Simon Brady <simonbrady@mac.com>
2018-01-14 15:18:53 -0500

    remove css, style links next

--- src/components/employers.js
+++ src/components/employers.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Employers extends React.Component {
   render() {
     return (
-      <div>
+      <div className="page">
         <p>
          EMPLOYERS
         </p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,34 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import '../App.css';
 import AboutUs from '../components/aboutUs.js';
 import Employers from '../components/employers.js';
 import Candidates from '../components/candidates.js';
 import Home from '../components/home.js';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-// import TransitionGroup from 'react-transition-group/TransitionGroup';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import AnimatedRoutes from "../components/animatedRoutes";
-
-
-/* animation handler */
-// const firstChild = props => {
-//   const childrenArray = React.Children.toArray(props.childnren);
-//   return childrenArray[0] || null;
-//};
 
 /* webpage */
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ATP Options </h1>
-        </header>
-        <p className="App-intro">
-        </p>
-          <Router>
-            <AnimatedRoutes>
-            <div>
-              <ul>
-                <li><Link to="/">ATPOptions</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/employers">Employers</Link></li>
-                <li><Link to="/candidates">Candidates</Link></li>
-              </ul>
-
-              <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" exact component={AboutUs} />
-                <Route path="/employers" exact component={Employers} />
-                <Route path="/candidates" exact component={Candidates} />
-              </Switch>
+        <Router>
+          <div>
+            <div className="TopBar">
+                <Link to="/" activeClassName="active">ATPOptions</Link>
+                <Link to="/about" activeClassName="active">About</Link>
+                <Link to="/employers" activeClassName="active">Employers</Link>
+                <Link to="/candidates" activeClassName="active">Candidates</Link>
             </div>
+            <AnimatedRoutes>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" exact component={AboutUs} />
+              <Route path="/employers" exact component={Employers} />
+              <Route path="/candidates" exact component={Candidates} />
             </AnimatedRoutes>
-          </Router>
-      </div>
+          </div>
+        </Router>
+      </div>  
     );
   }
 }
