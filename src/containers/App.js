5342ca4af49f73008788df4c2f3131bf00c4207d
Simon Brady <simonbrady@mac.com>
2018-01-14 11:20:23 -0500

    get animations functioning

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import '../App.css';
-import AboutUs from '../components/aboutUs.js'
-import Employers from '../components/employers.js'
-import Candidates from '../components/candidates.js'
-import Home from '../components/home.js'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import AboutUs from '../components/aboutUs.js';
+import Employers from '../components/employers.js';
+import Candidates from '../components/candidates.js';
+import Home from '../components/home.js';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+// import TransitionGroup from 'react-transition-group/TransitionGroup';
+import AnimatedRoutes from "../components/animatedRoutes";
 
+
+/* animation handler */
+// const firstChild = props => {
+//   const childrenArray = React.Children.toArray(props.childnren);
+//   return childrenArray[0] || null;
+//};
+
+/* webpage */
 class App extends Component {
   render() {
     return (
@@ -17,23 +27,25 @@ class App extends Component {
         </header>
         <p className="App-intro">
         </p>
-        <Router>
-          <div>
-            <ul>
-              <li><Link to="/">ATPOptions</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/employers">Employers</Link></li>
-              <li><Link to="/candidates">Candidates</Link></li>
-            </ul>
+          <Router>
+            <AnimatedRoutes>
+            <div>
+              <ul>
+                <li><Link to="/">ATPOptions</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/employers">Employers</Link></li>
+                <li><Link to="/candidates">Candidates</Link></li>
+              </ul>
 
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/about" exact component={AboutUs} />
-              <Route path="/employers" exact component={Employers} />
-              <Route path="/candidates" exact component={Candidates} />
-            </Switch>
-          </div>
-        </Router>
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/about" exact component={AboutUs} />
+                <Route path="/employers" exact component={Employers} />
+                <Route path="/candidates" exact component={Candidates} />
+              </Switch>
+            </div>
+            </AnimatedRoutes>
+          </Router>
       </div>
     );
   }
